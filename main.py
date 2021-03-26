29be4a5004f057326f000fe5a17407c3abee8f0c
Joe Panetta <jlapnetta1681@icloud.com>
2021-03-25 22:03:06 -0400

    First ship is a square block and its on the baord, now to make it move

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import time
 import random
 
-# pygame.font.init()
+pygame.font.init()
 
 width, height = 750, 750
 WIN = pygame.display.set_mode((width, height))
@@ -29,16 +29,45 @@ BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
 YELLOW_LASER = pygame.image.load(
     os.path.join("assets", "pixel_laser_yellow.png"))
 # game backdrop
-BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")),(width, height) )
+BG = pygame.transform.scale(pygame.image.load(
+    os.path.join("assets", "background-black.png")), (width, height))
+
+
+class Ship:
+    def __init__(self, x, y, health=100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_img = None
+        self.laser_img = None
+        self.lasers = []
+        self.cool_down_counter = 0
+
+    def draw(self, window):
+        pygame.draw.rect(window, (255, 0, 0), (self.x, self.y, 50, 50))
 
 
 def main():
     run = True
     FPS = 60
+    level = 1
+    lives = 5
+    main_font = pygame.font.SysFont("comicSans", 50)
+
+    ship = Ship(300, 650)
+
     clock = pygame.time.Clock()
 
     def redraw_window():
-        WIN.blit(BG,(0,0))
+        WIN.blit(BG, (0, 0))
+        level_label = main_font.render(f"Lives: {lives}", 5, (255, 0, 0))
+        lives_label = main_font.render(f"level: {level}", 1, (255, 0, 0))
+
+        ship.draw(WIN)
+
+        WIN.blit(level_label, (width - level_label.get_width() - 10, 10))
+        WIN.blit(lives_label, (10, 10))
+
         pygame.display.update()
 
     while run:
