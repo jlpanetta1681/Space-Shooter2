739c992b24780ec1fa4e1a10022c4d9ddc9c50db
Joe Panetta <jlapnetta1681@icloud.com>
2021-03-25 21:31:24 -0400

    Window background made. statring on adding the ships in

--- main.py
+++ main.py
@@ -10,20 +10,26 @@ WIN = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Super Spacy Invaders")
 
 # get our images into the file
-RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
-GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
-BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
+RED_SPACE_SHIP = pygame.image.load(
+    os.path.join("assets", "pixel_ship_red_small.png"))
+GREEN_SPACE_SHIP = pygame.image.load(
+    os.path.join("assets", "pixel_ship_green_small.png"))
+BLUE_SPACE_SHIP = pygame.image.load(
+    os.path.join("assets", "pixel_ship_blue_small.png"))
 
 # Yellow ship is player ship
-YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
+YELLOW_SPACE_SHIP = pygame.image.load(
+    os.path.join("assets", "pixel_ship_yellow.png"))
 
 # Lasers images
 RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
-GREEN_LASER = pygame.image.load(os.path.join("/assets", "pixel_laser_green.png"))
+GREEN_LASER = pygame.image.load(
+    os.path.join("assets", "pixel_laser_green.png"))
 BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
-YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
+YELLOW_LASER = pygame.image.load(
+    os.path.join("assets", "pixel_laser_yellow.png"))
 # game backdrop
-BG = pygame.image.load(os.path.join("assets", "background-black.png"))
+BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")),(width, height) )
 
 
 def main():
@@ -31,9 +37,13 @@ def main():
     FPS = 60
     clock = pygame.time.Clock()
 
+    def redraw_window():
+        WIN.blit(BG,(0,0))
+        pygame.display.update()
+
     while run:
         clock.tick(FPS)
-
+        redraw_window()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
